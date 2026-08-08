521561fcd598fe11c033b776b8b111e26cb0cf2c
JonasLuanPetry <jonas.petry@universo.univates.br>
2026-08-07 21:46:43 -0300

    novas atualizaçoes

--- aula31072/App.js
+++ aula31072/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Button, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Cavalo Manco Sírio</Text>
+      <Pressable>
+      <Text>Leitoa Caolha Chinesa</Text>
+      </Pressable>
       <StatusBar style="auto" />
+      <Button
+      title="Touro Monobola Azteca"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"/>
+      <TouchableHighlight
+      activeOpacity={0.6}
+      underlayColor="#DDDDDD">
+      <Text>Cabra Pelada Espanhola</Text>
+      </TouchableHighlight>
     </View>
   );
 }
